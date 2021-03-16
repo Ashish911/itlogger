c68db2c3650f11a0e350761bce2f90b8f7d36a05
Ashish <ashishDongol32@gmail.com>
2021-03-16 22:15:28 +0545

    tons of things done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Fragment, useEffect } from 'react';
 import SearcBar from './component/layout/SearchBar';
 import Logs from './component/logs/Logs';
 import AddBtn from './component/layout/AddBtn';
-import AddLogModal from './component/logs/AddLogModel';
+import AddLogModal from './component/logs/AddLogModal';
+import EditLogModal from './component/logs/EditLogModal';
+import AddTechModal from './component/techs/AddTechModal';
+import TechListModal from './component/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -22,6 +25,9 @@ const App = () => {
         <div className="container">
           <AddBtn/>
           <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
           <Logs/>
         </div>
       </Fragment>
