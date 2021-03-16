a104aa6d3e79abc07534104965703c57830aec5d
Ashish <ashishDongol32@gmail.com>
2021-03-16 21:59:30 +0545

    add log model added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import SearcBar from './component/layout/SearchBar';
 import Logs from './component/logs/Logs';
 import AddBtn from './component/layout/AddBtn';
+import AddLogModal from './component/logs/AddLogModel';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -20,6 +21,7 @@ const App = () => {
         <SearcBar />
         <div className="container">
           <AddBtn/>
+          <AddLogModal />
           <Logs/>
         </div>
       </Fragment>
